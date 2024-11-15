2c296a0e7dfc40e1d2ace603b3c9c595e54785fe
Blanco <katherinhe.blanco@accenture.com>
2024-11-15 13:08:29 -0500

    uploading new files

--- src/ReadingStringFromConsole.java
+++ src/ReadingStringFromConsole.java
@@ -1,9 +1,12 @@
-
 public class ReadingStringFromConsole {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+   public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter three words separated by spaces: ");
+       String s1 = input.next();
+       String s2 = input.next();
+       String s3 = input.next();
+       System.out.println("s1 is " + s1);
+       System.out.println("s2 is " + s2);
+       System.out.println("s3 is " + s3);
+   }
 }
--- src/ReadingStringFromConsole2.java
+++ src/ReadingStringFromConsole2.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class ReadingStringFromConsole2 {
+   public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+       try (Scanner input = new Scanner(System.in)) {
+		System.out.print("Enter a character: ");
+		   String s = input.nextLine();
+		   char ch = s.charAt(0);
+		   System.out.println("The character entered is " + ch);
 	}
-
+   }
 }
